8b062adbf2e1547b9122aabd0125e1e611be6e55
Alert175 <40724267+Alert175@users.noreply.github.com>
2021-06-18 15:52:25 +0300

    bag fix for optional revision v2 step - 4

--- src/components/general-components/Header/index.js
+++ src/components/general-components/Header/index.js
@@ -1,122 +1,125 @@
-import { Link, useLocation, useHistory } from "react-router-dom";
-import { useState } from "react";
+import {Link, useLocation, useHistory} from "react-router-dom";
+import {useState} from "react";
 import logo from "../../../assets/img/header-logo.png";
-import { useDispatch, useSelector } from "react-redux";
-import { changeFind } from "./findSlice";
-import { selectProducts } from "../../cart-components/Basket/basketSlice";
+import {useDispatch, useSelector} from "react-redux";
+import {changeFind} from "./findSlice";
+import {selectProducts} from "../../cart-components/Basket/basketSlice";
 
 const Header = () => {
-  // eslint-disable-next-line
-  const [links, setlinks] = useState([
-    {
-      text: "Главная",
-      url: "/",
-    },
-    {
-      text: "Каталог",
-      url: "/catalog",
-    },
-    {
-      text: "О магазине",
-      url: "/about",
-    },
-    {
-      text: "Контакты",
-      url: "/contacts",
-    },
-  ]);
-  const [showForm, setshowForm] = useState(false);
-  const [findWord, setfindWord] = useState("");
-  const location = useLocation();
-  const history = useHistory();
+    // eslint-disable-next-line
+    const [links, setlinks] = useState([
+        {
+            text: "Главная",
+            url: "/",
+        },
+        {
+            text: "Каталог",
+            url: "/catalog",
+        },
+        {
+            text: "О магазине",
+            url: "/about",
+        },
+        {
+            text: "Контакты",
+            url: "/contacts",
+        },
+    ]);
+    const [showForm, setshowForm] = useState(false);
+    const [findWord, setfindWord] = useState("");
+    const location = useLocation();
+    const history = useHistory();
 
-  // store state
-  const dispatch = useDispatch();
-  const productsByBasket = useSelector(selectProducts);
+    // store state
+    const dispatch = useDispatch();
+    const productsByBasket = useSelector(selectProducts);
 
-  const handlerBehavior = (event) => {
-    if (findWord !== "") {
-      handlerFind();
-    }
-    setshowForm(!showForm);
-  };
+    const handlerBehavior = (event) => {
+        if (findWord !== "") {
+            handlerFind();
+        }
+        setshowForm(!showForm);
+    };
 
-  const handlerFind = () => {
-    if (location !== "/catalog") {
-      history.push("/catalog");
-    }
-    dispatch(changeFind(findWord));
-  };
+    const handlerFind = () => {
+        if (location !== "/catalog") {
+            history.push("/catalog");
+        }
+        dispatch(changeFind(findWord));
+    };
 
-  const handlerSubmitFind = (event) => {
-    event.preventDefault();
-    handlerFind();
-  };
+    const handlerSubmitFind = (event) => {
+        event.preventDefault();
+        handlerFind();
+    };
 
-  return (
-    <header className="container">
-      <div className="row">
-        <div className="col">
-          <nav className="navbar navbar-expand-sm navbar-light bg-light">
-            <Link to="/" className="navbar-brand">
-              <img src={logo} alt="Bosa Noga" />
-            </Link>
-            <div className="collapase navbar-collapse" id="navbarMain">
-              <ul className="navbar-nav mr-auto">
-                {links.map((element, index) => (
-                  <li
-                    key={index}
-                    to={element.url}
-                    className={`nav-item ${
-                      location.pathname === element.url ? "active" : ""
-                    }`}
-                  >
-                    <Link to={element.url} className="nav-link">
-                      {element.text}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-              <div>
-                <div className="header-controls-pics">
-                  <div
-                    onClick={handlerBehavior}
-                    data-id="search-expander"
-                    className="header-controls-pic header-controls-search"
-                  ></div>
-                  <div
-                    className="header-controls-pic header-controls-cart"
-                    onClick={() => history.push("/cart")}
-                  >
-                    {productsByBasket.length > 0 && (
-                      <div className="header-controls-cart-full">
-                        {productsByBasket.length}
-                      </div>
-                    )}
-                    <div className="header-controls-cart-menu"></div>
-                  </div>
+    return (
+        <header className="container">
+            <div className="row">
+                <div className="col">
+                    <nav className="navbar navbar-expand-sm navbar-light bg-light">
+                        <Link to="/" className="navbar-brand">
+                            <img src={logo} alt="Bosa Noga"/>
+                        </Link>
+                        <div className="collapase navbar-collapse" id="navbarMain">
+                            <ul className="navbar-nav mr-auto">
+                                {links.map((element, index) => (
+                                    <li
+                                        key={index}
+                                        to={element.url}
+                                        className={`nav-item ${
+                                            location.pathname === element.url ? "active" : ""
+                                        }`}
+                                    >
+                                        <Link onClick={() => {
+                                            dispatch(changeFind(''))
+                                            setfindWord('')
+                                        }} to={element.url} className="nav-link">
+                                            {element.text}
+                                        </Link>
+                                    </li>
+                                ))}
+                            </ul>
+                            <div>
+                                <div className="header-controls-pics">
+                                    <div
+                                        onClick={handlerBehavior}
+                                        data-id="search-expander"
+                                        className="header-controls-pic header-controls-search"
+                                    ></div>
+                                    <div
+                                        className="header-controls-pic header-controls-cart"
+                                        onClick={() => history.push("/cart")}
+                                    >
+                                        {productsByBasket.length > 0 && (
+                                            <div className="header-controls-cart-full">
+                                                {productsByBasket.length}
+                                            </div>
+                                        )}
+                                        <div className="header-controls-cart-menu"></div>
+                                    </div>
+                                </div>
+                                <form
+                                    data-id="search-form"
+                                    className={`header-controls-search-form form-inline ${
+                                        !showForm ? "invisible" : ""
+                                    }`}
+                                    onSubmit={handlerSubmitFind}
+                                >
+                                    <input
+                                        className="form-control"
+                                        placeholder="Поиск"
+                                        value={findWord}
+                                        onInput={(event) => setfindWord(event.target.value)}
+                                    />
+                                </form>
+                            </div>
+                        </div>
+                    </nav>
                 </div>
-                <form
-                  data-id="search-form"
-                  className={`header-controls-search-form form-inline ${
-                    !showForm ? "invisible" : ""
-                  }`}
-                  onSubmit={handlerSubmitFind}
-                >
-                  <input
-                    className="form-control"
-                    placeholder="Поиск"
-                    value={findWord}
-                    onInput={(event) => setfindWord(event.target.value)}
-                  />
-                </form>
-              </div>
             </div>
-          </nav>
-        </div>
-      </div>
-    </header>
-  );
+        </header>
+    );
 };
 
 export default Header;
